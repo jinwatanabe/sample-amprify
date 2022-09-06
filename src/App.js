c590639824641b45081c1a83ba4a2bfe4b301bff
jinwatanabe <jin.watanabe.6g@gmail.com>
2022-09-06 19:04:45 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Amplify, Auth } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import aws_exports from "./aws-exports";
+
+Amplify.configure(aws_exports);
+
+const click = () => {
+  Auth.signOut();
+  window.location.reload();
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Sample React app</h1>
+        <h2>
+          <a className="App-link" href="." onClick={click}>
+            Sign Out
+          </a>
+        </h2>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
